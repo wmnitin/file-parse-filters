9786836355f4b22c3f7b32e8ca521f8a27272490
Nitin Kumar <wmnitin@gmail.com>
2020-05-13 09:52:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [fileData, fileDataChange] = useState(null)
+  const [delimiter, setDelimiter] = useState(',');
+  const [row, setRow] = useState(2);
+
+
+  const readFile = (event) => {
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      const text = (e.target.result)
+      parseFile(text)
+      // fileDataChange(text)
+    };
+    reader.readAsText(event.target.files[0])
+  }
+
+  const parseFile = (file) => {
+    const fileRowArr = file.split('\n');
+    fileDataChange(fileRowArr);
+  }
+
+  const changeDelimeter = (e) => {
+    setDelimiter(e.target.value)
+  }
+
+  const changeRow = (e) => {
+    setRow(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="FileUpload">
+        <input type="file" onChange={readFile} />
+      </div>
+      {
+        fileData && <>
+          <div>
+            <p>
+              <span>Delimiter: <input value={delimiter} onChange={changeDelimeter} /></span>
+              <span className="leftPadding">Lines: <input value={row} onChange={changeRow} /></span>
+            </p>
+          </div>
+          <table>{
+            fileData.map((file, index) => {
+              if (index < row) {
+                const fileArr = delimiter ? file.split(delimiter) : file.split(',');
+                return <tr>
+                  {
+                    fileArr.map((data) => {
+                      return <td>{data}</td>
+                    })
+                  }
+                </tr>
+              } else {
+                return null
+              }
+            })
+          }
+          </table>
+        </>
+      }
     </div>
   );
 }
